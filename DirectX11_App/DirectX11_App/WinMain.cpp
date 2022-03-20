078a8a92c712a561b56c03790ba5bb648fe474b4
jams10 <jamin3710@gmail.com>
2022-03-20 16:59:48 +0900

    02. 윈도우 클래스 등록 및 윈도우 생성

--- DirectX11_App/DirectX11_App/WinMain.cpp
+++ DirectX11_App/DirectX11_App/WinMain.cpp
@@ -1,10 +1,46 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR	  lpCmdLine,
-	int       nCmdShow)
+	HINSTANCE hInstance,     // 우리가 작성한 프로그램 인스턴스에 대한 핸들.
+	HINSTANCE hPrevInstance, // 현재는 사용하지 않음. 무조건 NULL.
+	LPSTR	  lpCmdLine,     // 명령줄. C프로그램 에서의 char** argv의 윈도우 애플리케이션 버전.
+	int       nCmdShow)      // 윈도우를 시작할 때의 형태. 최대/최소화 여부 등을 설정.
 {
-	return 0;
+	const wchar_t* pClassName = L"WindowClass";
+
+	// 윈도우 클래스 등록
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// 윈도우 생성
+	HWND hWnd = CreateWindowEx(
+		0,
+		pClassName,
+		L"윈도우!",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200,
+		640, 480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	// 윈도우 보이기
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
+	
+return 0;
 }
