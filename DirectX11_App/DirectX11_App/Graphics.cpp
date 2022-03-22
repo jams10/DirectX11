928e1f6426103d6f2a3472e53fed95fdb1d4e997
jams10 <jamin3710@gmail.com>
2022-03-22 21:37:34 +0900

    16(2). CPU와 GPU의 행렬 읽어들이는 순서 일치 시키기 / 변환 행렬의 x좌표에 화면의 종횡비를 곱해줌.

--- DirectX11_App/DirectX11_App/Graphics.cpp
+++ DirectX11_App/DirectX11_App/Graphics.cpp
@@ -188,10 +188,10 @@ void Graphics::DrawTestTriangle(float angle)
 	const ConstantBuffer cb =
 	{
 		{
-			std::cos(angle),	std::sin(angle),	0.0f,	0.0f,
-			-std::sin(angle),	std::cos(angle),	0.0f,	0.0f,
-			0.0f,				0.0f,				1.0f,	0.0f,
-			0.0f,				0.0f,				0.0f,	1.0f,
+			(3.0f / 4.0f) * std::cos(angle),	std::sin(angle),	0.0f,	0.0f,
+			(3.0f / 4.0f) * -std::sin(angle),	std::cos(angle),	0.0f,	0.0f,
+			0.0f,								0.0f,				1.0f,	0.0f,
+			0.0f,								0.0f,				0.0f,	1.0f,
 		}
 	};
 	Microsoft::WRL::ComPtr<ID3D11Buffer> pConstantBuffer;
