9d7e1cfecc329eeb3c3be1c0a8cf3de0fb53e000
jams10 <jamin3710@gmail.com>
2022-03-23 21:48:22 +0900

    20(2). 인덱스 버퍼 관련 에러 수정.

--- DirectX11_App/DirectX11_App/DrawableBase.h
+++ DirectX11_App/DirectX11_App/DrawableBase.h
@@ -6,20 +6,33 @@ template<class T>
 class DrawableBase : public Drawable
 {
 public:
-	bool IsStaticInitialized() const noexcept
+	static bool IsStaticInitialized() noexcept
 	{
 		return !staticBinds.empty(); // static 데이터들이 담긴 컨테이너가 비어 있으면, 아직 초기화를 해주지 않았음을 의미.
-	}
-	void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
+	} 
+	static void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
 	{
-		assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+		assert("*Must* use AddStaticIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
 		staticBinds.push_back(std::move(bind));
 	}
 	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept(!IS_DEBUG)
 	{
-		assert(pIndexBuffer == nullptr);
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
 		pIndexBuffer = ibuf.get();
 		staticBinds.push_back(std::move(ibuf));
+	}
+	void SetIndexFromStatic() noexcept(!IS_DEBUG)
+	{
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		for (const auto& b : staticBinds) // staticBinds를 순회하면서 인덱스 버퍼를 찾아 pIndexBuffer에 넣어줌.
+		{
+			if (const auto p = dynamic_cast<IndexBuffer*>(b.get()))
+			{
+				pIndexBuffer = p;
+				return;
+			}
+		}
+		assert("Failed to find index buffer in static binds" && pIndexBuffer != nullptr);
 	}
 private:
 	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept override
