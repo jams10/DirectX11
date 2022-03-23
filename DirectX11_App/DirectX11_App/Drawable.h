f2113e0eaab12f4a55264eb7b60977be96032007
jams10 <jamin3710@gmail.com>
2022-03-23 22:16:26 +0900

    20(3). 정점 상수 버퍼 클래스 객체를 인스턴스들이 공유 하도록 수정해줌.

--- DirectX11_App/DirectX11_App/Drawable.h
+++ DirectX11_App/DirectX11_App/Drawable.h
@@ -12,11 +12,13 @@ public:
 	Drawable() = default;
 	Drawable(const Drawable&) = delete;
 	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
-	void Draw(Graphics& gfx) const noexcept(!IS_DEBUG);
+	void Draw(Graphics & gfx) const noexcept(!IS_DEBUG);
 	virtual void Update(float dt) noexcept = 0;
+	virtual ~Drawable() = default;
+protected:
 	void AddBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG);
 	void AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept(!IS_DEBUG);
-	virtual ~Drawable() = default;
+private:
 	virtual const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept = 0;
 private:
 	const IndexBuffer* pIndexBuffer = nullptr; // 인덱스 버퍼의 경우 DrawIndexed 함수 호출 시 인덱스의 개수를 알아야 하므로, 따로 포인터로 들고 있어 줌.
--- DirectX11_App/DirectX11_App/DrawableBase.h
+++ DirectX11_App/DirectX11_App/DrawableBase.h
@@ -5,25 +5,25 @@
 template<class T>
 class DrawableBase : public Drawable
 {
-public:
+protected:
 	static bool IsStaticInitialized() noexcept
 	{
 		return !staticBinds.empty(); // static 데이터들이 담긴 컨테이너가 비어 있으면, 아직 초기화를 해주지 않았음을 의미.
 	} 
 	static void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG)
 	{
-		assert("*Must* use AddStaticIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+		assert("인덱스 버퍼를 bind 하기 위해서는 반드시 AddStaticIndexBuffer를 써야 함." && typeid(*bind) != typeid(IndexBuffer));
 		staticBinds.push_back(std::move(bind));
 	}
 	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept(!IS_DEBUG)
 	{
-		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		assert("인덱스 버퍼를 두 번 추가하려고 시도하고 있음." && pIndexBuffer == nullptr);
 		pIndexBuffer = ibuf.get();
 		staticBinds.push_back(std::move(ibuf));
 	}
 	void SetIndexFromStatic() noexcept(!IS_DEBUG)
 	{
-		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		assert("인덱스 버퍼를 두 번 추가하려고 시도하고 있음." && pIndexBuffer == nullptr);
 		for (const auto& b : staticBinds) // staticBinds를 순회하면서 인덱스 버퍼를 찾아 pIndexBuffer에 넣어줌.
 		{
 			if (const auto p = dynamic_cast<IndexBuffer*>(b.get()))
@@ -32,7 +32,7 @@ public:
 				return;
 			}
 		}
-		assert("Failed to find index buffer in static binds" && pIndexBuffer != nullptr);
+		assert("staticBinds에서 인덱스 버퍼를 찾을 수 없음." && pIndexBuffer != nullptr);
 	}
 private:
 	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept override
