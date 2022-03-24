765539771b45538388287f3a42057376627bd96e
jams10 <jamin3710@gmail.com>
2022-03-24 11:18:55 +0900

    20(4). 기본적인 도형 오브젝트를 그리기 위한 정점, 인덱스를 구성해주는 클래스 제작.

--- DirectX11_App/DirectX11_App/PixelShader.hlsl
+++ DirectX11_App/DirectX11_App/PixelShader.hlsl
@@ -5,5 +5,5 @@ cbuffer CBUF
 
 float4 main( uint tid : SV_PrimitiveID ) : SV_TARGET
 {
-	return face_colors[tid/2]; // 사각형 마다 두 개의 삼각형을 가지므로, 한 면이 단일 색상을 갖도록 tid / 2 인덱스로 접근.
+	return face_colors[(tid / 2) % 6];
 }
