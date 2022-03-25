f074e932be46316931681535db909aa61b57d6da
jams10 <jamin3710@gmail.com>
2022-03-25 12:40:51 +0900

    SkinnedBox 클래스에서 텍스쳐 이미지 자원을 사용하고, 픽셀 셰이더에서 샘플러를 사용하지만, 샘플러를 파이프라인에 묶어주지 않았던 부분을 수정함.

--- DirectX11_App/DirectX11_App/Drawable/SkinnedBox.cpp
+++ DirectX11_App/DirectX11_App/Drawable/SkinnedBox.cpp
@@ -4,6 +4,7 @@
 #include "Primitive\Cube.h"
 #include "Surface.h"
 #include "Texture.h"
+#include "Sampler.h"
 
 SkinnedBox::SkinnedBox(Graphics& gfx,
 	std::mt19937& rng,
@@ -41,6 +42,8 @@ SkinnedBox::SkinnedBox(Graphics& gfx,
 		AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
 
 		AddStaticBind(std::make_unique<Texture>(gfx, Surface::FromFile("Images\\cube.png")));
+
+		AddStaticBind(std::make_unique<Sampler>(gfx));
 
 		auto pvs = std::make_unique<VertexShader>(gfx, L"Shader\\TextureVS.cso");
 		auto pvsbc = pvs->GetBytecode();
