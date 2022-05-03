861afc2f400c7c3b7fb5466bedbf4fbed3c43650
jams10 <jamin3710@gmail.com>
2022-05-03 15:00:08 +0900

    30(9). pitch 값이 -90, 90이 될 경우 회전이 튀는 현상을 해걸.

--- DirectX11_App/DirectX11_App/Camera.cpp
+++ DirectX11_App/DirectX11_App/Camera.cpp
@@ -34,7 +34,7 @@ void Camera::SpawnControlWindow() noexcept
 		ImGui::SliderFloat("Y", &pos.y, -80.0f, 80.0f, "%.1f");
 		ImGui::SliderFloat("Z", &pos.z, -80.0f, 80.0f, "%.1f");
 		ImGui::Text("Orientation");
-		ImGui::SliderAngle("Pitch", &pitch, -90.0f, 90.0f);
+		ImGui::SliderAngle("Pitch", &pitch, 0.995f * -90.0f, 0.995f * 90.0f);
 		ImGui::SliderAngle("Yaw", &yaw, -180.0f, 180.0f);
 		if (ImGui::Button("Reset"))
 		{
@@ -54,7 +54,7 @@ void Camera::Reset() noexcept
 void Camera::Rotate(float dx, float dy) noexcept
 {
 	yaw = wrap_angle(yaw + dx * rotationSpeed); // y축 회전 값을 -180 ~ 180 값으로 제한해줌.
-	pitch = std::clamp(pitch + dy * rotationSpeed, -PI / 2.0f, PI / 2.0f); // x축 회전 값을 -90 ~ 90 값으로 제한.
+	pitch = std::clamp(pitch + dy * rotationSpeed, 0.995f * -PI / 2.0f, 0.995f * PI / 2.0f);
 }
 
 void Camera::Translate(DirectX::XMFLOAT3 translation) noexcept
