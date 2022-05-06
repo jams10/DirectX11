4926c43134d04be73c3e0b9d7551c008062193df
jams10 <jamin3710@gmail.com>
2022-05-06 14:04:35 +0900

    33(13). specular 맵 텍스쳐가 따로 없는 경우 material의 shininess 값을 가져와 specular power 값으로 사용하도록 함.

--- DirectX11_App/DirectX11_App/Mesh.cpp
+++ DirectX11_App/DirectX11_App/Mesh.cpp
@@ -270,6 +270,7 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 	std::vector<std::unique_ptr<Bind::Bindable>> bindablePtrs;
 
 	bool hasSpecularMap = false;
+	float shininess = 35.0f;
 	if (mesh.mMaterialIndex >= 0)
 	{
 		auto& material = *pMaterials[mesh.mMaterialIndex];
@@ -285,6 +286,10 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		{
 			bindablePtrs.push_back(std::make_unique<Bind::Texture>(gfx, Surface::FromFile(base + texFileName.C_Str()), 1));
 			hasSpecularMap = true;
+		}
+		else
+		{
+			material.Get(AI_MATKEY_SHININESS, shininess);
 		}
 
 		bindablePtrs.push_back(std::make_unique<Bind::Sampler>(gfx));
@@ -311,9 +316,10 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		struct PSMaterialConstant
 		{
 			float specularIntensity = 0.8f;
-			float specularPower = 40.0f;
+			float specularPower;
 			float padding[2];
 		} pmc;
+		pmc.specularPower = shininess;
 		bindablePtrs.push_back(std::make_unique<Bind::PixelConstantBuffer<PSMaterialConstant>>(gfx, pmc, 1u));
 	}
 
