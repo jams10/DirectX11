06e168d222bc2d4fdbb7bf38ead327f09a627760
jams10 <jamin3710@gmail.com>
2022-05-09 15:00:26 +0900

    34. 파이프라인 자원을 담아주는 bind 컨테이너를 shared_ptr 타입으로 변경함.

--- DirectX11_App/DirectX11_App/Drawable/Drawable.cpp
+++ DirectX11_App/DirectX11_App/Drawable/Drawable.cpp
@@ -12,24 +12,16 @@ void Drawable::Draw(Graphics& gfx) const noxnd
 	{
 		b->Bind(gfx);
 	}
-	for (auto& b : GetStaticBinds())
-	{
-		b->Bind(gfx);
-	}
 	gfx.DrawIndexed(pIndexBuffer->GetCount());
 }
 
-void Drawable::AddBind(std::unique_ptr<Bindable> bind) noxnd
+void Drawable::AddBind(std::shared_ptr<Bindable> bind) noxnd
 {
-	// 인덱스 버퍼를 넣으려고 하면 assert 발생 시킴.
-	assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+	// 인덱스 버퍼인 경우를 따로 체크해줌.
+	if (typeid(*bind) == typeid(IndexBuffer))
+	{
+		assert("Binding multiple index buffers not allowed" && pIndexBuffer == nullptr);
+		pIndexBuffer = &static_cast<IndexBuffer&>(*bind);
+	}
 	binds.push_back(std::move(bind));
-}
-
-void Drawable::AddIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noxnd
-{
-	// 인덱스 버퍼가 아닌 다른 것들을 넣으려고 하면 assert 발생 시킴.
-	assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
-	pIndexBuffer = ibuf.get(); // 인덱스 버퍼 포인터를 얻어와야 하기 때문에 인덱스 버퍼를 추가하는 함수만 따로 빼주었음.
-	binds.push_back(std::move(ibuf));
 }
--- DirectX11_App/DirectX11_App/Drawable/Drawable.h
+++ DirectX11_App/DirectX11_App/Drawable/Drawable.h
@@ -2,6 +2,7 @@
 #include "../Graphics.h"
 #include <DirectXMath.h>
 #include "../ConditionalNoexcept.h"
+#include <memory>
 
 namespace Bind
 {
@@ -11,14 +12,11 @@ namespace Bind
 
 class Drawable
 {
-	template<class T>
-	friend class DrawableBase;
 public:
 	Drawable() = default;
 	Drawable(const Drawable&) = delete;
 	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
 	void Draw(Graphics & gfx) const noxnd;
-	virtual void Update(float dt) noexcept {};
 	virtual ~Drawable() = default;
 protected:
 	template<class T>
@@ -33,11 +31,8 @@ protected:
 		}
 		return nullptr;
 	}
-	void AddBind(std::unique_ptr<Bind::Bindable> bind) noxnd;
-	void AddIndexBuffer(std::unique_ptr<Bind::IndexBuffer> ibuf) noxnd;
-private:
-	virtual const std::vector<std::unique_ptr<Bind::Bindable>>& GetStaticBinds() const noexcept = 0;
+	void AddBind(std::shared_ptr<Bind::Bindable> bind) noxnd;
 private:
 	const Bind::IndexBuffer* pIndexBuffer = nullptr; // 인덱스 버퍼의 경우 DrawIndexed 함수 호출 시 인덱스의 개수를 알아야 하므로, 따로 포인터로 들고 있어 줌.
-	std::vector<std::unique_ptr<Bind::Bindable>> binds;
+	std::vector<std::shared_ptr<Bind::Bindable>> binds;
 };
