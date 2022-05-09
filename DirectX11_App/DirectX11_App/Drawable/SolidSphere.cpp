ef70ff7ff9bd7d25758eb96bc6e6098f6485a0ca
jams10 <jamin3710@gmail.com>
2022-05-09 16:20:32 +0900

    34(6). Drawables 상속 클래스들이 codex를 이용하도록 수정함.

--- DirectX11_App/DirectX11_App/Drawable/SolidSphere.cpp
+++ DirectX11_App/DirectX11_App/Drawable/SolidSphere.cpp
@@ -11,14 +11,15 @@ SolidSphere::SolidSphere(Graphics& gfx, float radius)
 
 	auto model = Sphere::Make();
 	model.Transform(dx::XMMatrixScaling(radius, radius, radius));
-	AddBind(std::make_shared<VertexBuffer>(gfx, model.vertices));
-	AddBind(std::make_shared<IndexBuffer>(gfx, model.indices));
+	const auto geometryTag = "$sphere." + std::to_string(radius);
+	AddBind(VertexBuffer::Resolve(gfx, geometryTag, model.vertices));
+	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
 
-	auto pvs = std::make_shared<VertexShader>(gfx, "Shader\\SolidVS.cso");
-	auto pvsbc = pvs->GetBytecode();
+	auto pvs = VertexShader::Resolve(gfx, "Shader\\SolidVS.cso");
+	auto pvsbc = static_cast<VertexShader&>(*pvs).GetBytecode();
 	AddBind(std::move(pvs));
 
-	AddBind(std::make_shared<PixelShader>(gfx, "Shader\\SolidPS.cso"));
+	AddBind(PixelShader::Resolve(gfx, "Shader\\SolidPS.cso"));
 
 	struct PSColorConstant
 	{
@@ -26,11 +27,11 @@ SolidSphere::SolidSphere(Graphics& gfx, float radius)
 		float padding;
 	} colorConst;
 
-	AddBind(std::make_shared<PixelConstantBuffer<PSColorConstant>>(gfx, colorConst));
+	AddBind(PixelConstantBuffer<PSColorConstant>::Resolve(gfx, colorConst));
 
-	AddBind(std::make_shared<InputLayout>(gfx, model.vertices.GetLayout(), pvsbc));
+	AddBind(InputLayout::Resolve(gfx, model.vertices.GetLayout(), pvsbc));
 
-	AddBind(std::make_shared<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+	AddBind(Topology::Resolve(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
 
 	AddBind(std::make_shared<TransformCbuf>(gfx, *this));
 }
--- DirectX11_App/DirectX11_App/Mesh.cpp
+++ DirectX11_App/DirectX11_App/Mesh.cpp
@@ -226,6 +226,7 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 {
 	namespace dx = DirectX;
 	using TemplateVertex::VertexLayout;
+	using namespace Bind;
 
 	TemplateVertex::VertexBuffer vbuf(std::move(
 		VertexLayout{}
@@ -254,7 +255,10 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 		indices.push_back(face.mIndices[2]);
 	}
 
-	std::vector<std::shared_ptr<Bind::Bindable>> bindablePtrs;
+	std::vector<std::shared_ptr<Bindable>> bindablePtrs;
+
+	using namespace std::string_literals;
+	const auto base = "Models\\nano_textured\\"s;
 
 	bool hasSpecularMap = false;
 	float shininess = 35.0f;
@@ -262,16 +266,14 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 	{
 		auto& material = *pMaterials[mesh.mMaterialIndex];
 
-		using namespace std::string_literals;
-		const auto base = "Models\\nano_textured\\"s;
 		aiString texFileName;
 
 		material.GetTexture(aiTextureType_DIFFUSE, 0, &texFileName);
-		bindablePtrs.push_back(std::make_shared<Bind::Texture>(gfx, base + texFileName.C_Str()));
+		bindablePtrs.push_back(Texture::Resolve(gfx, base + texFileName.C_Str()));
 
 		if (material.GetTexture(aiTextureType_SPECULAR, 0, &texFileName) == aiReturn_SUCCESS)
 		{
-			bindablePtrs.push_back(std::make_shared<Bind::Texture>(gfx, base + texFileName.C_Str(), 1));
+			bindablePtrs.push_back(Texture::Resolve(gfx, base + texFileName.C_Str(), 1));
 			hasSpecularMap = true;
 		}
 		else
@@ -279,26 +281,28 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 			material.Get(AI_MATKEY_SHININESS, shininess);
 		}
 
-		bindablePtrs.push_back(std::make_shared<Bind::Sampler>(gfx));
+		bindablePtrs.push_back(Bind::Sampler::Resolve(gfx));
 	}
 
-	bindablePtrs.push_back(std::make_shared<Bind::VertexBuffer>(gfx, vbuf));
+	auto meshTag = base + "%" + mesh.mName.C_Str();
 
-	bindablePtrs.push_back(std::make_shared<Bind::IndexBuffer>(gfx, indices));
+	bindablePtrs.push_back(VertexBuffer::Resolve(gfx, meshTag, vbuf));
 
-	auto pvs = std::make_shared<Bind::VertexShader>(gfx, "Shader\\PhongVS.cso");
-	auto pvsbc = pvs->GetBytecode();
+	bindablePtrs.push_back(IndexBuffer::Resolve(gfx, meshTag, indices));
+
+	auto pvs = VertexShader::Resolve(gfx, "Shader\\PhongVS.cso");
+	auto pvsbc = static_cast<VertexShader&>(*pvs).GetBytecode();
 	bindablePtrs.push_back(std::move(pvs));
 
-	bindablePtrs.push_back(std::make_shared<Bind::InputLayout>(gfx, vbuf.GetLayout(), pvsbc));
+	bindablePtrs.push_back(InputLayout::Resolve(gfx, vbuf.GetLayout(), pvsbc));
 
 	if (hasSpecularMap)
 	{
-		bindablePtrs.push_back(std::make_shared<Bind::PixelShader>(gfx, "Shader\\PhongPSSpecMap.cso"));
+		bindablePtrs.push_back(PixelShader::Resolve(gfx, "Shader\\PhongPSSpecMap.cso"));
 	}
 	else
 	{
-		bindablePtrs.push_back(std::make_shared<Bind::PixelShader>(gfx, "Shader\\PhongPS.cso"));
+		bindablePtrs.push_back(PixelShader::Resolve(gfx, "Shader\\PhongPS.cso"));
 
 		struct PSMaterialConstant
 		{
@@ -307,7 +311,9 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 			float padding[2];
 		} pmc;
 		pmc.specularPower = shininess;
-		bindablePtrs.push_back(std::make_shared<Bind::PixelConstantBuffer<PSMaterialConstant>>(gfx, pmc, 1u));
+		// this is CLEARLY an issue... all meshes will share same mat const, but may have different
+		// Ns (specular power) specified for each in the material properties... bad conflict
+		bindablePtrs.push_back(PixelConstantBuffer<PSMaterialConstant>::Resolve(gfx, pmc, 1u));
 	}
 
 	return std::make_unique<Mesh>(gfx, std::move(bindablePtrs));
