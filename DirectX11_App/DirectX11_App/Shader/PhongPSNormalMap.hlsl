ba6ccf58a093d1e89d2ca6e9bf9fb0030418dbbf
jams10 <jamin3710@gmail.com>
2022-05-11 14:01:32 +0900

    35(4). 노말맵의 노말 값 좌표를 수정해 사용.

--- DirectX11_App/DirectX11_App/Shader/PhongPSNormalMap.hlsl
+++ DirectX11_App/DirectX11_App/Shader/PhongPSNormalMap.hlsl
@@ -28,7 +28,10 @@ float4 main(float3 worldPos : Position, float3 n : Normal, float2 tc : Texcoord)
     // 노말맵을 적용할 경우 노말맵 텍스쳐를 샘플링 하여 노말값을 얻어옴.
     if (normalMapEnabled)
     {
-        n = -nmap.Sample(splr, tc).xyz;
+        const float3 normalSample = nmap.Sample(splr, tc).xyz;
+        n.x = normalSample.x * 2.0f - 1.0f;
+        n.y = -normalSample.y * 2.0f + 1.0f;
+        n.z = -normalSample.z;
     }
 	// fragment to light vector data
     const float3 vToL = lightPos - worldPos;
