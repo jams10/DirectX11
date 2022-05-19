de61e3daf1e629836cc7055f34ab365d1b392e75
jams10 <jamin3710@gmail.com>
2022-05-19 15:54:35 +0900

    38(6). material 파라미터 조절 ui 추가.

--- DirectX11_App/DirectX11_App/Drawable/Drawable.h
+++ DirectX11_App/DirectX11_App/Drawable/Drawable.h
@@ -18,7 +18,7 @@ public:
 	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
 	void Draw(Graphics & gfx) const noxnd;
 	virtual ~Drawable() = default;
-protected:
+
 	template<class T>
 	T* QueryBindable() noexcept
 	{
@@ -31,6 +31,7 @@ protected:
 		}
 		return nullptr;
 	}
+protected:
 	void AddBind(std::shared_ptr<Bind::Bindable> bind) noxnd;
 private:
 	const Bind::IndexBuffer* pIndexBuffer = nullptr; // 인덱스 버퍼의 경우 DrawIndexed 함수 호출 시 인덱스의 개수를 알아야 하므로, 따로 포인터로 들고 있어 줌.
