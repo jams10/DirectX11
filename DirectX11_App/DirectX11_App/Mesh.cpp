6731f5fffca2b32163d4ac76d54164d8021c6f53
jams10 <jamin3710@gmail.com>
2022-05-19 16:24:21 +0900

    38(13). 텍스쳐 맵을 사용하지 않는 셰이더의 스페큘러 색상을 바꾸는 ui 추가.

--- DirectX11_App/DirectX11_App/Mesh.cpp
+++ DirectX11_App/DirectX11_App/Mesh.cpp
@@ -508,7 +508,7 @@ std::unique_ptr<Mesh> Model::ParseMesh(Graphics& gfx, const aiMesh& mesh, const 
 
 		Node::PSMaterialConstantNotex pmc;
 		pmc.specularPower = shininess;
-		pmc.specularIntensity = (specularColor.x + specularColor.y + specularColor.z) / 3.0f;
+		pmc.specularColor = specularColor;
 		pmc.materialColor = diffuseColor;
 		// this is CLEARLY an issue... all meshes will share same mat const, but may have different
 		// Ns (specular power) specified for each in the material properties... bad conflict
