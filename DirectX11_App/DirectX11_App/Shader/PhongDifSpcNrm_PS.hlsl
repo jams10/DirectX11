6d001e725f179dad6ade32e5d524c0d193c29c4c
jams10 <jamin3710@gmail.com>
2022-06-22 11:57:19 +0900

    48(8). 고블린 모델 테스트.

--- DirectX11_App/DirectX11_App/Shader/PhongDifSpcNrm_PS.hlsl
+++ DirectX11_App/DirectX11_App/Shader/PhongDifSpcNrm_PS.hlsl
@@ -4,12 +4,12 @@
 
 cbuffer ObjectCBuf
 {
-    bool normalMapEnabled;
-    bool specularMapEnabled;
-    bool hasGloss;
-    float specularPowerConst;
+    bool useGlossAlpha;
     float3 specularColor;
-    float specularMapWeight;
+    float specularWeight;
+    float specularGloss;
+    bool useNormalMap;
+    float normalMapWeight;
 };
 
 Texture2D tex;
@@ -37,7 +37,7 @@ float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float3 vi
     // 메쉬의 노말 값을 정규화
     viewNormal = normalize(viewNormal);
     // 노말맵을 적용할 경우 노말맵 텍스쳐를 샘플링 하여 노말값을 얻어오고, 이 노말 벡터 값을 사용함.
-    if (normalMapEnabled)
+    if (useNormalMap)
     {
         viewNormal = MapNormal(normalize(viewTan), normalize(viewBitan), viewNormal, tc, nmap, splr);
     }
@@ -46,19 +46,12 @@ float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float3 vi
     const LightVectorData lv = CalculateLightVectorData(viewLightPos, viewFragPos);
     // specular 파라미터를 결정함.(이미지 맵을 사용할 것인지 정해진 상수 값을 사용할 것인지)
     float3 specularReflectionColor;
-    float specularPower = specularPowerConst;
-    if (specularMapEnabled)
+    float specularPower = specularGloss;
+    const float4 specularSample = spec.Sample(splr, tc);
+    specularReflectionColor = specularSample.rgb;
+    if (useGlossAlpha)
     {
-        const float4 specularSample = spec.Sample(splr, tc);
-        specularReflectionColor = specularSample.rgb * specularMapWeight;
-        if (hasGloss)
-        {
-            specularPower = pow(2.0f, specularSample.a * 13.0f);
-        }
-    }
-    else
-    {
-        specularReflectionColor = specularColor;
+        specularPower = pow(2.0f, specularSample.a * 13.0f);
     }
     // 감쇠 값
     const float att = Attenuate(attConst, attLin, attQuad, lv.distToL);
@@ -66,10 +59,10 @@ float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float3 vi
     const float3 diffuse = Diffuse(diffuseColor, diffuseIntensity, att, lv.dirToL, viewNormal);
     // 반사된 specular 
     const float3 specularReflected = Speculate(
-        specularReflectionColor, 1.0f, viewNormal,
+        diffuseColor * diffuseIntensity * specularReflectionColor, specularWeight, viewNormal,
         lv.vToL, viewFragPos, att, specularPower
     );
 
     // 난반사 텍스쳐 색상을 통해 감쇠된 난반사, 주변광 색상에 반사된 정반사광을 더해줌.
-    return float4(saturate((diffuse + ambient) * dtex.rgb + specularReflected), dtex.a);
+    return float4(saturate((diffuse + ambient) * dtex.rgb + specularReflected), 1.0f);
 }
