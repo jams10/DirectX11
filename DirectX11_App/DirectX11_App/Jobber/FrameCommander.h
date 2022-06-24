28a364fc1e4e80d383439fc2233b41f7fa36feba
jams10 <jamin3710@gmail.com>
2022-06-24 12:18:15 +0900

    49(2). offscreen을 이용한 negatvie filtering

--- DirectX11_App/DirectX11_App/Jobber/FrameCommander.h
+++ DirectX11_App/DirectX11_App/Jobber/FrameCommander.h
@@ -6,14 +6,36 @@
 #include "Pass.h"
 #include "PerfLog.h"
 #include "DepthStencil.h"
+#include "RenderTarget.h"
+#include <array>
 
 class FrameCommander
 {
 public:
 	FrameCommander(Graphics& gfx)
 		:
-		ds(gfx, gfx.GetWidth(), gfx.GetHeight())
-	{}
+		ds(gfx, gfx.GetWidth(), gfx.GetHeight()),
+		rt(gfx, gfx.GetWidth(), gfx.GetHeight())
+	{
+		namespace dx = DirectX;
+
+		// setup fullscreen geometry
+		TemplateVertex::VertexLayout lay;
+		lay.Append(TemplateVertex::VertexLayout::Position2D);
+		TemplateVertex::VertexBuffer bufFull{ lay };
+		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,1 });
+		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,1 });
+		bufFull.EmplaceBack(dx::XMFLOAT2{ -1,-1 });
+		bufFull.EmplaceBack(dx::XMFLOAT2{ 1,-1 });
+		pVbFull = Bind::VertexBuffer::Resolve(gfx, "$Full", std::move(bufFull));
+		std::vector<unsigned short> indices = { 0,1,2,1,3,2 };
+		pIbFull = Bind::IndexBuffer::Resolve(gfx, "$Full", std::move(indices));
+
+		// setup fullscreen shaders
+		pPsFull = Bind::PixelShader::Resolve(gfx, "Shader\\Funk_PS.cso");
+		pVsFull = Bind::VertexShader::Resolve(gfx, "Shader\\Fullscreen_VS.cso");
+		pLayoutFull = Bind::InputLayout::Resolve(gfx, lay, pVsFull->GetBytecode());
+	}
 	void Accept(Job job, size_t target) noexcept
 	{
 		passes[target].Accept(job);
@@ -25,9 +47,9 @@ public:
 		// and later on it would be a complex graph with parallel execution contingent
 		// on input / output requirements
 
-		// setup render target used for all passes
+		// setup render target used for normal passes
 		ds.Clear(gfx);
-		gfx.BindSwapBuffer(ds);
+		rt.BindAsTarget(gfx, ds);
 
 		// main phong lighting pass
 		Stencil::Resolve(gfx, Stencil::Mode::Off)->Bind(gfx);
@@ -39,6 +61,15 @@ public:
 		// outline drawing pass
 		Stencil::Resolve(gfx, Stencil::Mode::Mask)->Bind(gfx);
 		passes[2].Execute(gfx);
+		// fullscreen funky pass
+		gfx.BindSwapBuffer();
+		rt.BindAsTexture(gfx, 0);
+		pVbFull->Bind(gfx);
+		pIbFull->Bind(gfx);
+		pVsFull->Bind(gfx);
+		pPsFull->Bind(gfx);
+		pLayoutFull->Bind(gfx);
+		gfx.DrawIndexed(pIbFull->GetCount());
 	}
 	void Reset() noexcept
 	{
@@ -48,6 +79,19 @@ public:
 		}
 	}
 private:
+	static TemplateVertex::VertexLayout MakeFullscreenQuadLayout()
+	{
+		TemplateVertex::VertexLayout lay;
+		lay.Append(TemplateVertex::VertexLayout::Position2D);
+		return lay;
+	}
+private:
 	std::array<Pass, 3> passes;
 	DepthStencil ds;
+	RenderTarget rt;
+	std::shared_ptr<Bind::VertexBuffer> pVbFull;
+	std::shared_ptr<Bind::IndexBuffer> pIbFull;
+	std::shared_ptr<Bind::VertexShader> pVsFull;
+	std::shared_ptr<Bind::PixelShader> pPsFull;
+	std::shared_ptr<Bind::InputLayout> pLayoutFull;
 };
