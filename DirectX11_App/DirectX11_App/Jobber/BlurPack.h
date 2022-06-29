c61e19a5cf6ea81e811b371841b4aa5d6249247c
jams10 <jamin3710@gmail.com>
2022-06-29 09:46:55 +0900

    51(2). 가우시안 필터 사용해보기.

--- DirectX11_App/DirectX11_App/Jobber/BlurPack.h
+++ DirectX11_App/DirectX11_App/Jobber/BlurPack.h
@@ -5,9 +5,9 @@
 class BlurPack
 {
 public:
-	BlurPack(Graphics& gfx, int radius = 5, float sigma = 1.0f)
+	BlurPack(Graphics& gfx, int radius = 7, float sigma = 2.6f)
 		:
-		shader(gfx, "Blur_PS.cso"),
+		shader(gfx, "Shader\\Blur_PS.cso"),
 		pcb(gfx, 0u),
 		ccb(gfx, 1u)
 	{
@@ -35,14 +35,14 @@ public:
 		Kernel k;
 		k.nTaps = radius * 2 + 1;
 		float sum = 0.0f;
-		for (int i = 1; i < k.nTaps + 1; i++)
+		for (int i = 0; i < k.nTaps; i++)
 		{
-			const auto x = float(i - (radius + 1));
+			const auto x = float(i - radius);
 			const auto g = gauss(x, sigma);
 			sum += g;
 			k.coefficients[i].x = g;
 		}
-		for (int i = 1; i < k.nTaps + 1; i++)
+		for (int i = 0; i < k.nTaps; i++)
 		{
 			k.coefficients[i].x /= sum; // coefficient의 값의 합을 1로 만들어주기 위해 정규화.
 		}
@@ -52,11 +52,13 @@ private:
 	struct Kernel
 	{
 		int nTaps; // 커널의 너비.
+		float padding[3];
 		DirectX::XMFLOAT4 coefficients[15]; // 정렬로 인해 float4를 사용함.
 	};
 	struct Control
 	{
 		BOOL horizontal; // 셰이더에게 수평으로 처리하는 지 수직으로 처리하는 지 알려줌.
+		float padding[3];
 	};
 	Bind::PixelShader shader;
 	Bind::PixelConstantBuffer<Kernel> pcb;  // 커널을 위한 상수 버퍼
